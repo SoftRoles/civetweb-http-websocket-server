1da08b5fb13ce302d44c35f6599e1dd48be58423
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-02-20 18:42:04 +0300

    websocket server added

--- html/js/main.js
+++ html/js/main.js
@@ -1,3 +1,29 @@
-setTimeout(function(){
-  alert(1000);
-},1000)
+setTimeout(function () {
+  // alert(1000);
+
+  let socket = new WebSocket(`ws://${location.host}/websocket`);
+
+  socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    console.log("Sending to server");
+    socket.send("My name is Hüseyin");
+  };
+
+  socket.onmessage = function (event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function (error) {
+    console.log(`[error] ${error.message}`);
+  };
+}, 1000)
--- main.c
+++ main.c
@@ -63,7 +63,6 @@ AssetHandler(struct mg_connection *conn, void *cbdata)
 {
   const struct mg_request_info *ri = mg_get_request_info(conn);
 
-
   const char *folderName = (const char *)cbdata;
   char *result = malloc(strlen(folderName) + strlen(ri->local_uri) + 1); // +1 for the null-terminator
   // in real code you would check for errors in malloc here
@@ -78,6 +77,195 @@ AssetHandler(struct mg_connection *conn, void *cbdata)
   return 1;
 }
 
+#define USE_WEBSOCKET
+#ifdef USE_WEBSOCKET
+
+/* MAX_WS_CLIENTS defines how many clients can connect to a websocket at the
+ * same time. The value 5 is very small and used here only for demonstration;
+ * it can be easily tested to connect more than MAX_WS_CLIENTS clients.
+ * A real server should use a much higher number, or better use a dynamic list
+ * of currently connected websocket clients. */
+#define MAX_WS_CLIENTS (5)
+
+struct t_ws_client
+{
+  /* Handle to the connection, used for mg_read/mg_write */
+  struct mg_connection *conn;
+
+  /*
+	    WebSocketConnectHandler sets state to 1 ("connected")
+	    the connect handler can accept or reject a connection, but it cannot
+	    send or receive any data at this state
+
+	    WebSocketReadyHandler sets state to 2 ("ready")
+	    reading and writing is possible now
+
+	    WebSocketCloseHandler sets state to 0
+	    the websocket is about to be closed, reading and writing is no longer
+	    possible this callback can be used to cleanup allocated resources
+
+	    InformWebsockets is called cyclic every second, and sends some data
+	    (a counter value) to all websockets in state 2
+	*/
+  int state;
+} static ws_clients[MAX_WS_CLIENTS];
+
+#define ASSERT(x)                              \
+  {                                            \
+    if (!(x))                                  \
+    {                                          \
+      fprintf(stderr,                          \
+              "Assertion failed in line %u\n", \
+              (unsigned)__LINE__);             \
+    }                                          \
+  }
+
+int WebSocketConnectHandler(const struct mg_connection *conn, void *cbdata)
+{
+  struct mg_context *ctx = mg_get_context(conn);
+  int reject = 1;
+  int i;
+
+  mg_lock_context(ctx);
+  for (i = 0; i < MAX_WS_CLIENTS; i++)
+  {
+    if (ws_clients[i].conn == NULL)
+    {
+      ws_clients[i].conn = (struct mg_connection *)conn;
+      ws_clients[i].state = 1;
+      mg_set_user_connection_data(ws_clients[i].conn,
+                                  (void *)(ws_clients + i));
+      reject = 0;
+      break;
+    }
+  }
+  mg_unlock_context(ctx);
+
+  fprintf(stdout,
+          "Websocket client %s\r\n\r\n",
+          (reject ? "rejected" : "accepted"));
+  return reject;
+}
+
+void WebSocketReadyHandler(struct mg_connection *conn, void *cbdata)
+{
+  const char *text = "Hello from the websocket ready handler";
+  struct t_ws_client *client = mg_get_user_connection_data(conn);
+
+  mg_websocket_write(conn, MG_WEBSOCKET_OPCODE_TEXT, text, strlen(text));
+  fprintf(stdout, "Greeting message sent to websocket client\r\n\r\n");
+  ASSERT(client->conn == conn);
+  ASSERT(client->state == 1);
+
+  client->state = 2;
+}
+
+int WebsocketDataHandler(struct mg_connection *conn,
+                         int bits,
+                         char *data,
+                         size_t len,
+                         void *cbdata)
+{
+  struct t_ws_client *client = mg_get_user_connection_data(conn);
+  ASSERT(client->conn == conn);
+  ASSERT(client->state >= 1);
+
+  fprintf(stdout, "Websocket got %lu bytes of ", (unsigned long)len);
+  switch (((unsigned char)bits) & 0x0F)
+  {
+  case MG_WEBSOCKET_OPCODE_CONTINUATION:
+    fprintf(stdout, "continuation");
+    break;
+  case MG_WEBSOCKET_OPCODE_TEXT:
+    fprintf(stdout, "text");
+    break;
+  case MG_WEBSOCKET_OPCODE_BINARY:
+    fprintf(stdout, "binary");
+    break;
+  case MG_WEBSOCKET_OPCODE_CONNECTION_CLOSE:
+    fprintf(stdout, "close");
+    break;
+  case MG_WEBSOCKET_OPCODE_PING:
+    fprintf(stdout, "ping");
+    break;
+  case MG_WEBSOCKET_OPCODE_PONG:
+    fprintf(stdout, "pong");
+    break;
+  default:
+    fprintf(stdout, "unknown(%1xh)", ((unsigned char)bits) & 0x0F);
+    break;
+  }
+  fprintf(stdout, " data:\r\n");
+  fwrite(data, len, 1, stdout);
+  fprintf(stdout, "\r\n\r\n");
+
+  return 1;
+}
+
+void WebSocketCloseHandler(const struct mg_connection *conn, void *cbdata)
+{
+  struct mg_context *ctx = mg_get_context(conn);
+  struct t_ws_client *client = mg_get_user_connection_data(conn);
+  ASSERT(client->conn == conn);
+  ASSERT(client->state >= 1);
+
+  mg_lock_context(ctx);
+  while (client->state == 3)
+  {
+    /* "inform" state, wait a while */
+    mg_unlock_context(ctx);
+#ifdef _WIN32
+    Sleep(1);
+#else
+    usleep(1000);
+#endif
+    mg_lock_context(ctx);
+  }
+  client->state = 0;
+  client->conn = NULL;
+  mg_unlock_context(ctx);
+
+  fprintf(stdout,
+          "Client dropped from the set of webserver connections\r\n\r\n");
+}
+
+void InformWebsockets(struct mg_context *ctx)
+{
+  static unsigned long cnt = 0;
+  char text[32];
+  size_t textlen;
+  int i;
+
+  sprintf(text, "%lu", ++cnt);
+  textlen = strlen(text);
+
+  for (i = 0; i < MAX_WS_CLIENTS; i++)
+  {
+    int inform = 0;
+
+    mg_lock_context(ctx);
+    if (ws_clients[i].state == 2)
+    {
+      /* move to "inform" state */
+      ws_clients[i].state = 3;
+      inform = 1;
+    }
+    mg_unlock_context(ctx);
+
+    if (inform)
+    {
+      mg_websocket_write(ws_clients[i].conn,
+                         MG_WEBSOCKET_OPCODE_TEXT,
+                         text,
+                         textlen);
+      mg_lock_context(ctx);
+      ws_clients[i].state = 2;
+      mg_unlock_context(ctx);
+    }
+  }
+}
+#endif
+
 int main(int argc, char *argv[])
 {
   const char *options[] = {"listening_ports",
@@ -88,6 +276,10 @@ int main(int argc, char *argv[])
                            "error.log",
                            "enable_auth_domain_check",
                            "no",
+#ifdef USE_WEBSOCKET
+                           "websocket_timeout_ms",
+                           "3600000",
+#endif
                            0};
 
   struct mg_callbacks callbacks;
@@ -95,7 +287,19 @@ int main(int argc, char *argv[])
   int err = 0;
 
   mg_init_library(0);
+  if (err)
+  {
+    fprintf(stderr, "Cannot start CivetWeb - inconsistent build.\n");
+    return EXIT_FAILURE;
+  }
 
+  if (!mg_check_feature(16))
+  {
+    fprintf(stderr,
+            "Error: Embedded example built with websocket support, "
+            "but civetweb library build without.\n");
+    err = 1;
+  }
   if (err)
   {
     fprintf(stderr, "Cannot start CivetWeb - inconsistent build.\n");
@@ -123,7 +327,16 @@ int main(int argc, char *argv[])
   /* Show sone info */
   printf("Start example: %s%s\n", HOST_INFO, EXAMPLE_URI);
   printf("Exit example:  %s%s\n", HOST_INFO, EXIT_URI);
-
+#ifdef USE_WEBSOCKET
+  /* WS site for the websocket connection */
+  mg_set_websocket_handler(ctx,
+                           "/websocket",
+                           WebSocketConnectHandler,
+                           WebSocketReadyHandler,
+                           WebsocketDataHandler,
+                           WebSocketCloseHandler,
+                           0);
+#endif
   /* Wait until the server should be closed */
   while (!exitNow)
   {
@@ -131,6 +344,9 @@ int main(int argc, char *argv[])
     Sleep(1000);
 #else
     sleep(1);
+#endif
+#ifdef USE_WEBSOCKET
+    InformWebsockets(ctx);
 #endif
   }
 
